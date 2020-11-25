3c7f2e9fdaf1200662e157a4af941fbe62b2f5ad
lidaoyi@T6 <noVioletEvergarden@163.com>
2020-11-25 22:47:21 +0800

    2020/11/25 22:30 实现基本文法

---
+++ Lab_2/CodeTable.cpp
@@ -0,0 +1,63 @@
+
+/*----------------- 标识符 -----------------*/
+const int TOKEN = 1;
+/*----------------- 常数 -----------------*/
+const int SIGNEDINT = 2;
+const int SIGNEDFLOAT = 3;
+const int BOOLEAN = 4;
+const int STRING = 5;
+/*----------------- C语言关键字 -----------------*/
+//const int AUTO = 6;
+const int BREAK = 7;
+//const int CASE = 8;
+const int CHAR = 9;
+//const int CONST = 10;
+//const int CONTINUE = 11;
+//const int DEFAULT = 12;
+//const int DO = 13;
+const int DOUBLE = 14;
+const int ELSE = 15;
+//const int ENUM = 16;
+//const int EXTERN = 17;
+//const int FLOAT = 18;
+//const int FOR = 19;
+//const int GOTO = 20;
+const int IF = 21;
+const int INT = 22;
+//const int LONG = 23;
+//const int REGISTER = 24;
+const int RETURN = 25;
+//const int SHORT = 26;
+//const int SIGNED = 27;
+//const int SIZEOF = 28;
+//const int STATIC = 29;
+//const int STRUCT = 30;
+//const int SWITCH = 31;
+//const int TYPEDEF = 32;
+//const int UNION = 33;
+//const int UNSIGNED = 34;
+//const int VOID = 35;
+//const int VOLATILE = 36;
+const int WHILE = 37;
+/*----------------- 运算符 -----------------*/
+const int ADD = 38;
+const int SUB = 39;
+const int MUL = 40;
+const int DIV = 41;
+const int NOT = 42;
+const int OR = 43;
+const int AND = 44;
+const int EQU = 45; // ==
+const int NEQ = 46; // !=
+const int GOE = 47; // >=
+const int SOE = 48; // <=
+const int GRT = 49; // >
+const int SML = 50; // <
+const int ASSIGN = 57; // =
+/*----------------- 分界符 -----------------*/
+// const int COMMA = 51;     // ,
+const int SEMICOLON = 52; // ;
+const int LP = 53;        // (
+const int RP = 54;        // )
+// const int LC = 55;        // {
+// const int RC = 56;        // }
