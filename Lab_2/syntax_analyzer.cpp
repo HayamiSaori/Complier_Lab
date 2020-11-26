9eab471463477611d44805271610cd9d706c0119
Li Daoyi <819574743@qq.com>
2020-11-26 12:06:35 +0800

    2020/11/26 12:00 commit

--- Lab_2/syntax_analyzer.cpp
+++ Lab_2/syntax_analyzer.cpp
@@ -4,8 +4,13 @@
 #include <ctype.h>
 #include <fstream>
 #include "code_table.cpp"
-
+using namespace std;
+int ParseToken(string token)
+{
+    
+}
 int main(int argc,char * argv[])
 {
+    
     return 0;
 }
